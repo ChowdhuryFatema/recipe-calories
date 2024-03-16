c24b2d94b14c21e122026ef65100bd7ac92b9a62
ChowdhuryFatema <fatemachowdhury318@gmail.com>
2024-03-16 08:18:33 -0700

    Changed lorem ipsum

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -5,15 +5,16 @@ const Banner = () => {
         <div className="container mx-auto max-w-[1180px] px-5">
             <div className="hero py-12 md:py-20 lg:py-24 bg-no-repeat bg-cover bg-center rounded-2xl">
                 <div className="hero-content text-center text-neutral-content">
-                    <div className="w-full md:w-4/5 lg:w-2/3 space-y-8">
+                    <div className="w-full md:w-4/5 lg:w-2/3 space-y-5">
                         <h1 className="mb-5 text-2xl md:text-4xl lg:text-5xl font-bold text-white leading-[40px] md:leading-[50px] lg:leading-[65px]">Discover an exceptional cooking class tailored for you!</h1>
-                        <p className="mb-5">Embark on a culinary journey like no other with our bespoke cooking classes, curated just for you, to refine your skills and tantalize your taste buds. Experience the joy of creating delectable dishes in a personalized setting tailored to your preferences.</p>
+                        <p className="pb-5">
+                        Experience the art of cooking like never before with our customized classes, crafted to cater to your unique preferences and elevate your culinary expertise.</p>
 
                         <button className="btn bg-red-500 mr-2 md:mr-5 hover:bg-red-600 px-5 text-white  text-sm md:text-lg rounded-full border-0 duration-500">Explore Now</button>
 
                         <button className="btn bg-transparent hover:bg-red-500 hover:border-red-500 px-5 text-white text-sm md:text-lg rounded-full duration-500">Our Feedback</button>
 
-                        
+
                     </div>
                 </div>
             </div>
--- src/components/Recipes/Recipes.jsx
+++ src/components/Recipes/Recipes.jsx
@@ -7,25 +7,25 @@ import 'react-toastify/dist/ReactToastify.css';
 
 const Recipes = () => {
 
-     const [recipes, setRecipes] = useState([]);
-     const [carts, setCarts] = useState([]);
-     const [preparing, setPreparing] = useState([]);
-     const [totalTime, setTotalTime] = useState(0);
-     const [totalCalories, setCalories] = useState(0);
+    const [recipes, setRecipes] = useState([]);
+    const [carts, setCarts] = useState([]);
+    const [preparing, setPreparing] = useState([]);
+    const [totalTime, setTotalTime] = useState(0);
+    const [totalCalories, setCalories] = useState(0);
 
-     useEffect(() => {
+    useEffect(() => {
         fetch('./recipes.json')
-        .then(res => res.json())
-        .then(data => setRecipes(data))
+            .then(res => res.json())
+            .then(data => setRecipes(data))
     }, []);
 
     const handleWantToCook = cart => {
         const isExists = carts.find(c => c.recipe_id == cart.recipe_id)
-        if(!isExists){
+        if (!isExists) {
             setCarts([...carts, cart])
             toast.success("Successfully Added")
         }
-        else{
+        else {
             toast.warn("Already exist")
         }
     }
@@ -36,7 +36,7 @@ const Recipes = () => {
         toast.success("Added For Preparing")
         setPreparing([...preparing, prepare])
 
-        const prepareingTime  = parseInt(prepare.preparing_time);
+        const prepareingTime = parseInt(prepare.preparing_time);
         setTotalTime(totalTime + prepareingTime)
         const caloriesNum = parseInt(prepare.calories)
         setCalories(totalCalories + caloriesNum)
@@ -46,27 +46,27 @@ const Recipes = () => {
         <div className="container mx-auto max-w-[1180px] px-5">
             <div className="my-12 *:text-center space-y-5">
                 <h2 className="font-semibold text-2xl md:text-3xl lg:text-4xl">Our Recipes</h2>
-                <p className="w-full md:w-4/5 lg:w-3/5 mx-auto text-[#150B2B99]">Lorem ipsum dolor sit amet consectetur. Proin et feugiat senectus vulputate netus pharetra rhoncus. Eget urna volutpat curabitur elementum mauris aenean neque. </p>
+                <p className="w-full md:w-4/5 lg:w-3/5 mx-auto text-[#150B2B99]">Embark on a flavorful journey with our diverse collection of meticulously crafted recipes, igniting your passion for cooking and satisfying every craving.</p>
             </div>
             <div className="grid grid-cols-1 lg:grid-cols-12 gap-5">
                 <div className="col-auto lg:col-span-7">
                     <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
-                    {
-                        recipes.map(recipe => <Recipe 
-                        key={recipe.recipe_id}
-                        recipe={recipe} 
-                        handleWantToCook={handleWantToCook}>
-                        </Recipe>)
-                    }
+                        {
+                            recipes.map(recipe => <Recipe
+                                key={recipe.recipe_id}
+                                recipe={recipe}
+                                handleWantToCook={handleWantToCook}>
+                            </Recipe>)
+                        }
                     </div>
                 </div>
                 <div className="col-auto lg:col-span-5">
-                   <Cart 
-                   carts={carts}  
-                   handlePreparing={handlePreparing}
-                   totalTime={totalTime} 
-                   preparing={preparing} 
-                   totalCalories={totalCalories}></Cart>
+                    <Cart
+                        carts={carts}
+                        handlePreparing={handlePreparing}
+                        totalTime={totalTime}
+                        preparing={preparing}
+                        totalCalories={totalCalories}></Cart>
                 </div>
             </div>
         </div>
